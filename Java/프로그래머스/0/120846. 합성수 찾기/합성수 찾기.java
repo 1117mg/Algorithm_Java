0957b387212930180fade5882afa6897dfa65786
Yelim Lee <142207312+1117mg@users.noreply.github.com>
2025-01-27 22:32:18 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 87.6 MB -BaekjoonHub

--- Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ Java/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,17 +1,26 @@
+import java.util.Arrays;
+
 class Solution {
-	public static int solution(int n) {
+    public static int solution(int n) {
         int answer = 0;
-        for (int i = 4; i <= n;i++) {
-            int count = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true); // 모든 수를 소수로 초기화
+        isPrime[0] = isPrime[1] = false; // 0과 1은 소수가 아님
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) { // i가 소수라면
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false; // i의 배수는 합성수로 처리
                 }
             }
-            if (count > 2) {
+        }
+
+        for (int i = 4; i <= n; i++) {
+            if (!isPrime[i]) { // 소수가 아닌 수는 합성수
                 answer++;
             }
         }
+
         return answer;
     }
 }
